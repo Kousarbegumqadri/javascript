b51a9dc1f123a5035c0ffded9bfc803495d8712c
Kousarbegumqadri <134119314+Kousarbegumqadri@users.noreply.github.com>
2025-02-04 15:01:10 +0000

    container config done

---
+++ 01_basics/variable.js
@@ -0,0 +1,7 @@
+const accountId=1234
+var accountNum=2233
+let accountEmail="hi@gmail.com"
+//const can't be changed
+accountNum=3333
+let accountCity;
+console.table([accountId,accountNum,accountEmail,accountNum,accountCity])
