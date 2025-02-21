807344693ed74ff6c335b89791288606bf06276f
Kousarbegumqadri <134119314+Kousarbegumqadri@users.noreply.github.com>
2025-02-21 15:05:12 +0000

    numbers

---
+++ 01_basics/numbers.js
@@ -0,0 +1,24 @@
+// const score=400;
+// console.log(score);
+
+// const num=new Number(400);
+// console.log(num);
+// console.log(num.toString().length);
+// console.log(num.toFixed(1));
+
+// const num1=123.8999
+// console.log(num1.toPrecision(3));
+
+// const hun=10000000
+// console.log(hun.toLocaleString('en-IN'));
+
+////////////////////////////MATHS//////////////
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9))
+// console.log(Math.min(3,5,2,1));
+// console.log(Math.max(4,6,2,1,3));
+console.log(Math.random())
