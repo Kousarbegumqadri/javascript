cb8f7ac1539c9ef3a3ab2727e24d809daa20cdbc
Kousarbegumqadri <134119314+Kousarbegumqadri@users.noreply.github.com>
2025-02-25 11:34:12 +0000

    arrays

---
+++ 02.basics/arrays.js
@@ -0,0 +1,26 @@
+///Arrays
+const myArr=[0,2,3,1,4]
+const myheros=["athiya","talat"]
+const myArr2=new Array(1,2,3,4)
+// console.log(myArr[1])
+/***********************Methods***********************/
+
+// myArr.push(6)
+// myArr.pop()
+// console.log(myArr)
+// myArr.unshift(4)
+// console.log(myArr)
+// myArr.shift()
+// console.log(myArr)
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(3))
+//  const newArr=myArr.join()
+// console.log(myArr)
+// console.log(newArr)
+// console.log("A ",myArr)
+// const ar=myArr.slice(1,3)
+// console.log(myArr)
+// console.log("B ",myArr)
+const arrr=myArr.splice(1,3)
+console.log(myArr)
+console.log("B ",myArr)
