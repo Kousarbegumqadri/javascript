97d86700990afc342202e63fc18bd6f4df9cdb25
Kousarbegumqadri <134119314+Kousarbegumqadri@users.noreply.github.com>
2025-02-25 08:42:15 +0000

    dateAndTime

---
+++ 01_basics/date_and_time.js
@@ -0,0 +1,8 @@
+/////////// Dates /////////
+// let my = new Date()
+// console.log(my.toString())
+// console.log(my.toDateString())
+// console.log(my.toLocaleDateString());
+
+let myCred=new Date(2023,0,23)
+console.log(myCred.toDateString())
