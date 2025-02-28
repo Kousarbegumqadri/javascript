9333f8f689f6e66c0bccb9cdc81e48719fffbd1e
Kousarbegumqadri <134119314+Kousarbegumqadri@users.noreply.github.com>
2025-02-28 14:50:07 +0000

    arrays.js

---
+++ 02.basics/2-arrays.js
@@ -0,0 +1,15 @@
+// const alfa=["a",'b','c','d']
+// const alfa2=['e','f','g','h']
+// //alfa.push(alfa2)
+// //console.log(alfa)
+// // const new_array=alfa.concat(alfa2)
+// // console.log(new_array)
+// const anotherArray=[1,2,3,[3,4,5],[4,5],[5,6]]
+// const realArray=anotherArray.flat(Infinity)
+// console.log(realArray)
+// console.log(Array.isArray("Hitesh"))
+// console.log(Array.from("Hitesh"))
+let score1=100
+let score2=200
+let score3=300
+console.log(Array.of(score1,score2,score3))
