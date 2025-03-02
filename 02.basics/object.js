621b63ed063fd2c29180993fb3874b61fff30ab5
Kousarbegumqadri <134119314+Kousarbegumqadri@users.noreply.github.com>
2025-03-02 07:45:26 +0000

    object.js

--- 02.basics/object.js
+++ 02.basics/object.js
@@ -1,7 +1,24 @@
-const User={
+mysum=Symbol('key1')
+ const User={
     name:'athiya',
+    [mysum]:'mykey1',
     age:18,
     location:'india'
 }
 console.log(User.location)
+console.log(typeof User[mysum])
 //console.log(User[location])
+User.name='talat';
+//Object.freeze(User);
+User.name='sonu';
+console.log(User)
+//// Adding Greetings to USer
+User.greetings=function(){
+    console.log("hello js user");
+}
+User.greetingTwo=function(){
+    console.log('hello js user,${this.name}')
+}
+//getting error
+console.log(User.greetings());
+console.log(User.greetingTwo());
