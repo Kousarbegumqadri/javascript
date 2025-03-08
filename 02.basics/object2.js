ba3090407a6cd94acc5e1fa6e1e589aac4575044
Kousarbegumqadri <134119314+Kousarbegumqadri@users.noreply.github.com>
2025-03-08 09:55:33 +0000

    object2

---
+++ 02.basics/object2.js
@@ -0,0 +1,39 @@
+// const tinderuser=new Object()
+const tinderuser={}
+tinderuser.id='b182188'
+tinderuser.name='athiya'
+tinderuser.location='hyd'
+
+console.log(tinderuser);
+const regularuser={
+    email:'kousar@',
+    fullname: {
+        username:{
+            firstname:'athiya',
+            lastname:'talat'
+        }
+    }
+}
+//console.log(regularuser.fullname)
+const obj1={1:'a',2:'b'}
+const obj2={3:'a',4:'d'}
+//const obj3={obj1,obj2}
+//const obj3=Object.assign({},obj1,obj2)
+const obj3={...obj1,...obj2}
+//console.log(obj3)
+
+//*********************array objects */
+const user=[
+    {
+        id:12,
+        user:'athi',
+    },
+    {
+        number:2,
+        name:'ab',
+    },
+]
+console.log(tinderuser);
+console.log(Object.keys(tinderuser))
+console.log(Object.values(tinderuser))
+console.log(Object.entries(tinderuser))
