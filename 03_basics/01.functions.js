0a505aa35bd1ce8985bf91328645a1b60cf9022e
Kousarbegumqadri <134119314+Kousarbegumqadri@users.noreply.github.com>
2025-03-08 13:30:35 +0000

    functions

--- 03_basics/01.functions.js
+++ 03_basics/01.functions.js
@@ -1,27 +1,43 @@
-function say(){
-    // console.log('a');
-    // console.log('f');
-    // console.log('b');
-}
-say();
-function addTwoNum(number1,number2){ //parameters//
-    //console.log(number1+number2);
-}
-function addTwoNum(number1,number2){ //parameters//
- return number1+number2;
-}
+// function say(){
+//     // console.log('a');
+//     // console.log('f');
+//     // console.log('b');
+// }
+//say();
+// function addTwoNum(number1,number2){ //parameters//
+//     //console.log(number1+number2);
+// }
+// function addTwoNum(number1,number2){ //parameters//
+//  return number1+number2;
+// }
 // const result=addTwoNum(3,5);
 // console.log("result :",result);
 // addTwoNum(3,8) //arguments//
 // addTwoNum(3,'4')
 // addTwoNum(3,'a')
-function isLogin(username){
-    if(username==='undefined'){
-        console.log('please enter username');
-        return;      //after writing return the next line wont execute;//
-    }
-    return '${username} just loggedin';
-}
+// function isLogin(username){
+//     if(username==='undefined'){
+//         console.log('please enter username');
+//         return;      //after writing return the next line wont execute;//
+//     }
+//     return '${username} just loggedin';
+// }
 //console.log(isLogin('athiya'));             ///getting error in windows//
 //console.log(isLogin());                 //the output will get user i=undefind
-console.log(isLogin())
+//console.log(isLogin())
+
+
+//***********Shopping card***************** *//
+// const user={
+//     username:'athiya',
+//     cardnum:199,
+// }
+// function handleObject(anyobject){
+//     console.log('username is ${anyobject.username},cardnum is ${anyobject.cardnum}');
+// }
+// handleObject(user);
+//************************************************* */
+const myNewArray=[200,400,500,300];
+function returnSecondValue(getArray){
+    
+}
